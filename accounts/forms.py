3851556d93887d5baaadad287797a81f13d1eae6
amiramomen <amiramomen999@gmail.com>
2021-09-08 03:03:34 +0100

    profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from .models import Profile
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm 
@@ -6,3 +7,16 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields =['username', 'password1', 'password2', 'email']
+
+
+
+class Userform (forms.ModelForm):
+    class Meta:
+        model = User
+        fields =['first_name', 'last_name' , 'email']
+
+class Profileform (forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields =['phone', 'image','city']
+        
